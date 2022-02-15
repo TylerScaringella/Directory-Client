847f5f64d5b62285096e9aa2c95daa1ef98355e9
TylerScaringella <=>
2022-02-15 16:18:49 -0500

    initial commit

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="container">
+    <view class="cards">
+      <text v-if="students.length == 0">Loading directory...</text>
+      <Card v-else v-for="student in students" :key="student.email" :student="student" />
+    </view>
+  </view>
+</template>
+
+<script>
+import axios from 'axios'
+
+import Card from './components/Card'
+
+export default {
+    data() {
+      return {
+        punches: 0,
+        students: []
+      }
+    },
+    components: {
+      Card
+    },
+    mounted() {
+      axios.get('http://10.1.232.108:8081/student').then(students => {
+        this.students = students.data
+      })
+    },
+    methods: {
+    },
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <view>
+        <image :style="{width: 200, height: 200}" :source="{uri: `${student.picture}`}" />
+        <text>Name: {{student.name}}</text>
+        <text>Email: {{student.email}}</text>
+    </view>
+</template>
+
+<script>
+export default {
+    props: {
+        student: {}
+    }
+}
+</script>
+
+<style>
+
+</style>
