8b33f61e6fac5357497e732c4602a7ae61f471d5
TylerScaringella <=>
2022-02-23 11:45:57 -0500

    updates

--- App.vue
+++ App.vue
@@ -1,8 +1,28 @@
 <template>
   <view class="container">
     <view class="cards">
-      <text v-if="students.length == 0">Loading directory...</text>
-      <Card v-else v-for="student in students" :key="student.email" :student="student" />
+      <view class="filters">
+        <text-input 
+          :style="{height: 50, borderColor: 'gray', borderWidth: 2}"
+          v-model="filter.name"
+          placeholder="enter a name..."
+          :on-value-change="renderedStudents = students.filter(student => student.name.toLowerCase().includes(filter.name.toLowerCase()))"
+        />
+        <view class="classes">
+          <button 
+            :on-press="updateGrad(2022)"
+            title="2022"
+            color="#841584"
+            accessibility-label="Click to see the class of 2025"
+          />
+        </view>
+      </view>
+      <activity-indicator v-if="students.length == 0" :style="{flex: 1, justifyContent: 'center'}" size="large" color="#0000ff" />
+      <scroll-view v-else :content-container-style="{contentContainer: {
+        paddingVertical: 0
+      }}">
+        <Card v-for="student in renderedStudents" :key="student.email" :student="student" />
+      </scroll-view>
     </view>
   </view>
 </template>
@@ -11,24 +31,36 @@
 import axios from 'axios'
 
 import Card from './components/Card'
+import { StyleSheet } from 'react-native'
 
 export default {
     data() {
       return {
         punches: 0,
-        students: []
+        filter: {
+          name: '',
+          grad: 0
+        },
+        form: '',
+        students: [],
+        renderedStudents: []
       }
     },
     components: {
-      Card
+      Card,
+      StyleSheet
+    },
+    methods: {
+      updateGrad(year) {
+        this.filter.grad = year
+      }
     },
     mounted() {
       axios.get('http://10.1.232.108:8081/student').then(students => {
         this.students = students.data
+        this.renderedStudents = this.students
       })
-    },
-    methods: {
-    },
+    }
 }
 </script>
 
@@ -41,5 +73,9 @@ export default {
 }
 .text-color-primary {
   color: blue;
+}
+
+.cards {
+  margin-top: 50px;
 }
 </style>
--- components/Card.vue
+++ components/Card.vue
@@ -1,8 +1,10 @@
 <template>
-    <view>
-        <image :style="{width: 200, height: 200}" :source="{uri: `${student.picture}`}" />
-        <text>Name: {{student.name}}</text>
-        <text>Email: {{student.email}}</text>
+    <view class="card">
+        <text class="name">{{student.name}}</text>
+        <text class="email">{{student.email}}</text>
+        <text class="graduation" :style="{
+            textAlign: 'right'
+        }">Class of {{student.graduation}}</text>
     </view>
 </template>
 
@@ -15,5 +17,29 @@ export default {
 </script>
 
 <style>
+.card {
+    border-width: 2px;
+    border-color: gray;
+    border-radius: 5px;
+    padding: 15px;
+    margin-top: 30px;
+    position: relative;
+}
 
+.name {
+    font-weight: 500;
+    font-size: 20px;
+}
+
+.email {
+    color: gray;
+    font-size: 10px;
+    font-weight: 200;
+}
+
+.graduation {
+    font-size: 10px;
+    font-weight: 200;
+    margin-top: 30px;
+}
 </style>
